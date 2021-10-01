72b74e63d9de8a6277d75d956e56632a6d3b2f9c
shoebham <25881429+shoebham@users.noreply.github.com>
2021-10-01 18:49:17 +0530

    added tab change function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,58 @@
-import insertContent from "./homepage";
+import home from "./homepage";
+import menu from "./menu";
+import contact from "./contact";
+import {init} from "./init";
 
-insertContent();
+
+function changeContent()
+{
+    const ul = document.querySelectorAll("li");
+    ul.forEach(li=>
+        {
+            if(li.classList.contains("selected"))
+            {
+                change(`${li.classList[0]}`);
+            }
+        });
+}
+
+function change(li)
+{
+    clear();
+    if(li=="Home")
+        home();
+    if(li=="Menu")
+        menu();
+    if(li=="Contact")
+        contact();
+}
+
+function clear()
+{
+    const li = document.querySelector(`.selected`);
+    const selected = li.classList[0];
+    console.log(selected);
+    const div = document.querySelector(`.${selected}-div`);
+    console.log(div);
+    div.innerHTML="";
+
+    div.remove();
+}
+
+function changeListener()
+{
+    const ul = document.querySelectorAll("li");
+    ul.forEach((li)=>
+    {
+    console.log(li);
+
+        li.addEventListener("click",()=>
+        {
+           change(li.classList[0]);
+        })
+    })
+}
+init();
+home();
+// changeListener();
+export default change;
